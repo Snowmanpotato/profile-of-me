4891d8c68fc341ee490f08b415d91c3095f01709
Nguyen Chon Loi <chonloi2561998@gamil.com>
2022-08-12 15:15:29 +0700

    Event

---
+++ HTML/HTML/CSS_Basic/Event/move.js
@@ -0,0 +1,16 @@
+function goUp(){
+    let moveUp = document.getElementById("nobita");
+    moveUp.style.top = parseInt(moveUp.style.top) -5 + "px";
+}
+function goDown(){
+    let moveDown = document.getElementById("nobita");
+    moveUp.style.top = parseInt(moveUp.style.top) +5 + "px";
+}
+function goLeft(){
+    let moveUp = document.getElementById("nobita");
+    moveUp.style.left = parseInt(moveUp.style.left) -5 + "px";
+}
+function goRight(){
+    let moveUp = document.getElementById("nobita");
+    moveUp.style.left = parseInt(moveUp.style.left) +5 + "px";
+}
