9e3af24f1404db0715f0d773c59ae2a7307b7021
Nguyen Chon Loi <chonloi2561998@gamil.com>
2022-08-22 15:13:59 +0700

    HTML

--- HTML/HTML/nope.js
+++ HTML/HTML/nope.js
@@ -1,4 +1,19 @@
-let x = [1, 3, 5, 7];
-let y = [2, 4, 6];
-x[2] = y[x.length - 3];
-console.log(x);
+let houseWork = [
+    'quét nhà',
+    'rửa bát',
+    'nấu cơm',
+    'đón con',
+    'cho gà ăn',
+    'lau nhà',
+    'giặt áo quần',
+    'phơi áo quần',
+    'quét sân',
+    'tưới cây'
+];
+let job = document.querySelector('#job');
+for (let w = 0; w < houseWork.length; w++) {
+    job.innerHTML += `<tr>
+                        <td>${w + 1}</td>
+                        <td>${houseWork[w]}</td>
+                    </tr>`
+}
